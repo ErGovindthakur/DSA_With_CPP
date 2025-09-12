e11b7e02df17c8c97cdb8b65cd7df0b50c6a99b9
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-13 01:07:33 +0530

    solve reverse array problem with recursion and binary approach

--- 5_BasicRecursion/07_ReverseArrayWithRec.cpp
+++ 5_BasicRecursion/07_ReverseArrayWithRec.cpp
@@ -10,6 +10,26 @@ void reverseArray(int arr[], int newArr[], int index, int n)
      reverseArray(arr, newArr, index + 1, n);
 }
 
+void reverseArrayWithBinaryApproach(int arr[], int start, int end)
+{
+     if (start > end)
+          return;
+
+     int temp = arr[start];
+     arr[start] = arr[end];
+     arr[end] = temp;
+
+     reverseArrayWithBinaryApproach(arr, start + 1, end - 1);
+}
+
+void printRevArrayWithBinApp(int arr[], int n)
+{
+     cout << "Rev array with Binary approach -: " << endl;
+     for (int i = 0; i < n; i++)
+     {
+          cout << arr[i] << " ";
+     }
+}
 int main()
 {
      int myArr[] = {1, 2, 3, 4, 5};
@@ -21,5 +41,8 @@ int main()
      {
           cout << newArr[i] << " ";
      }
+     cout << endl;
+     reverseArrayWithBinaryApproach(myArr, 0, n - 1);
+     printRevArrayWithBinApp(myArr, n);
      return 0;
 }
