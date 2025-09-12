fb07fab5fdcef50eb1427cc98a50b5072095e019
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-12 23:55:16 +0530

    solve sum of n numbers problem using recursion

---
+++ 5_BasicRecursion/05_PrintSumOfNnumbers.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int printSumOfNnumbers(int num){
+     if(num>7)return 0;
+     return num+printSumOfNnumbers(num+1);
+}
+int main() {
+     int num = 1;
+     int sumOfNnumbers = printSumOfNnumbers(num);
+     cout<<sumOfNnumbers<<endl;
+     return 0;
+}
