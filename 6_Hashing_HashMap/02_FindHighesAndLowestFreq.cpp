b105e7bc0bf905198ff673747b000024f60a47f7
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-13 14:29:46 +0530

    improve code readability

--- 6_Hashing_HashMap/02_FindHighesAndLowestFreq.cpp
+++ 6_Hashing_HashMap/02_FindHighesAndLowestFreq.cpp
@@ -7,8 +7,8 @@ void highestAndLowestFreqElem(vector<int> myVec)
 {
      map<int, int> freq;
 
-     int maxFreq = (int)-INFINITY;
-     int minFreq = (int)INFINITY;
+     int maxFreq = INT_MIN;
+     int minFreq = INT_MAX;
      // cout << " MaxFreq -: " << maxFreq << " MinFreq -: " << minFreq << endl;
 
      for (int elem : myVec)
@@ -16,30 +16,46 @@ void highestAndLowestFreqElem(vector<int> myVec)
           freq[elem]++;
      }
 
+     // finding max and min freq
      for (auto &it : freq)
      {
-          // cout << it.first << " -> " << it.second << endl;
-          if (it.second > maxFreq)
-          {
-               maxFreq = it.second;
-               if (it.second == maxFreq)
-               {
-                    cout<<"From maxFreq"<<endl;
-                    cout << it.first << " -> " << it.second << endl;
-               }
-          }
-          if (it.second < minFreq)
-          {
-               minFreq = it.second;
-               if (it.second == minFreq)
-               {
-                    cout<<"From minFreq"<<endl;
-                    cout << it.first << " -> " << it.second << endl;
-               }
-          }
+          maxFreq = max(maxFreq,it.second);
+          minFreq = min(minFreq,it.second);
+          // if (it.second > maxFreq)
+          // {
+          //      maxFreq = it.second;
+          //      if (it.second == maxFreq)
+          //      {
+          //           cout<<"From maxFreq"<<endl;
+          //           cout << it.first << " -> " << it.second << endl;
+          //      }
+          // }
+          // if (it.second < minFreq)
+          // {
+          //      minFreq = it.second;
+          //      if (it.second == minFreq)
+          //      {
+          //           cout<<"From minFreq"<<endl;
+          //           cout << it.first << " -> " << it.second << endl;
+          //      }
+          // }
      }
 
-     cout << " MaxFreq -: " << maxFreq << " MinFreq -: " << minFreq << endl;
+     // printing max and min freq
+
+     cout<<"Max freq elem \n";
+     for(auto &it:freq){
+          if(it.second==maxFreq){
+               cout<<it.first<<" -> "<<it.second<<endl;
+          }
+     }
+     cout<<"Min freq elem \n";
+     for(auto &it:freq){
+          if(it.second==minFreq){
+               cout<<it.first<<" -> "<<it.second<<endl;
+          }
+     }
+     cout << "MaxFreq -: " << maxFreq << " MinFreq -: " << minFreq << endl;
 }
 int main()
 {
