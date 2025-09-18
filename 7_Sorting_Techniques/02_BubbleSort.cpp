6e58477e5c8345bbf5bf518a6018bff56e983df3
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-18 23:26:26 +0530

    improve code efficiency

--- 7_Sorting_Techniques/02_BubbleSort.cpp
+++ 7_Sorting_Techniques/02_BubbleSort.cpp
@@ -3,17 +3,21 @@ using namespace std;
 
 void bubbleSort(int arr[],int n){
      for(int i = 0; i<n; i++){
+          // adding flag to break the code if array is already sorted
+          bool isSwapped = false;
           cout<<"The value of i -> "<<i<<endl;
-          for(int j = 0; j<n-i; j++){
+          for(int j = 0; j<n-i-1; j++){
                // cout<<arr[j]<<", ";
                // cout<<"The value of j -: "<<j<<endl;
                if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    isSwapped = true;
                }
           }
-          // cout<<endl;
+          cout<<"Runs"<<endl;
+          if(!isSwapped)break;
      }
 }
 void printArr(int arr[],int n){
