212cd8bc6150b29e1564988423f815b3d6b90fcc
ErGovindthakur <ergovindthakur@gmail.com>
2025-09-18 22:56:10 +0530

    fixes some minor bugs

--- 7_Sorting_Techniques/01_SelectionSort.cpp
+++ 7_Sorting_Techniques/01_SelectionSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void selectionSort(int arr[],int n){
      for(int i = 0; i<n-1; i++){
           int min = i;
-          for(int j = i; j<n-1; j++){
+          for(int j = i; j<n; j++){
                if(arr[j]<arr[min]){
                     min = j;
                     // cout<<"Min Value -: "<<min<<endl;
