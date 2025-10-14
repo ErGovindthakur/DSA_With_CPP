49e3d328848de96e5db16661c301a5cfc0076534
ErGovindthakur <ergovindthakur@gmail.com>
2025-10-14 18:35:20 +0530

    solve two sum problem using optimized approach

--- 10_25ArraysBestProblem/problemLists/01_TwoSum/TwoSumBetterApproach.java
+++ 10_25ArraysBestProblem/problemLists/01_TwoSum/TwoSumBetterApproach.java
@@ -1,6 +1,30 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+class TwoSum {
+     HashMap<Integer, Integer> map = new HashMap<>();
+
+     int[] twoSum(int[] nums, int target) {
+          for (int i = 0; i < nums.length; i++) {
+               int diff = target - nums[i];
+               if (map.containsKey(diff)) {
+                    return new int[] { map.get(diff), i };
+               }
+               map.put(nums[i], i);
+          }
+          return null;
+     }
+}
 
 public class TwoSumBetterApproach {
      public static void main(String[] args) {
           System.out.println("Working");
+
+          TwoSum sum = new TwoSum();
+          int[] nums = { 2, 7, 11, 15 };
+          int target = 9;
+
+          int[] result = sum.twoSum(nums, target);
+          System.out.println("Indices are: " + Arrays.toString(result));
      }
 }
