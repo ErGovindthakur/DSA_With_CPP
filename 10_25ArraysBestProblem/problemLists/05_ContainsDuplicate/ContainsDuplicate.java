83effddb74e2839f9a4337d77e676730d17e16ac
ErGovindthakur <ergovindthakur@gmail.com>
2025-10-15 07:38:13 +0530

    solve find duplicate value using optimal approach

---
+++ 10_25ArraysBestProblem/problemLists/05_ContainsDuplicate/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+class IsDuplicateValue{
+     boolean checkDuplicateOptimal(int []arr){
+
+          HashSet<Integer> hashElem = new HashSet<>();
+
+          // applying loop over array
+          for(int i = 0; i<arr.length; i++){
+               if(hashElem.contains(arr[i])){
+                    return true;
+               }
+               hashElem.add(arr[i]);
+          }
+          return false;
+     }
+}
+public class ContainsDuplicate {
+     public static void main(String[] args) {
+          int [] dupElem = {1,9,2,3,5};
+
+          IsDuplicateValue dup = new IsDuplicateValue();
+
+          boolean isDup = dup.checkDuplicateOptimal(dupElem);
+
+          System.out.println("Duplicate elem is available -> "+isDup);
+     }
+}
