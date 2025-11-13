7605f45218737f7b0dabd5687e4eae03c9b41c00
ErGovindthakur <ergovindthakur@gmail.com>
2025-11-14 00:42:57 +0530

    explore how find find node where cycle is started from

---
+++ 13_LinkedList_InterviewQuestion/09_LLCycleStart/LLCycleStartNode.java
@@ -0,0 +1,38 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LLCycleStartNode {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+
+                ListNode temp = head;
+
+                while (temp != slow) {
+                    temp = temp.next;
+                    slow = slow.next;
+
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
